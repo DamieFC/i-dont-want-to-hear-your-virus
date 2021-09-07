10b8121c42d6e0504bbdfa8d220abd3ecc683fc0
GNUOverYooouu <dtortoise99@gmail.com>
2021-09-07 09:16:07 -0700

    Yay the virus itself

---
+++ src/idwthyv.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  while(1) {
+    printf("Don't tell me\n ");
+    printf("I don't want to hear your problems\n");
+    printf("Don't tell me\n");
+    printf("I don't want to hear your shit\n");
+  }
+}
